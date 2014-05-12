2ffc12ff910c54c082862dff38ffd091ce0891f3
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-05-13 01:38:51 +0900

    begin 3-3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -19,7 +19,7 @@ describe "Static pages" do
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('Abut Us')
+      expect(page).to have_content('About Us')
     end
   end
 

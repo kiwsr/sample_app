a6ffaeec1603a62b2479e9f5ffcdd7b492b80a91
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-05-25 13:40:53 +0900

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       # flash[:success] = "Welcome to the Sample App!"
       flash[:success] = "ようこそ！ユーザー登録は正常に完了しました！"
       redirect_to @user
